d7fc5f3202b6298b0f5ff2ff573a6fa1a66fa4b4
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-06-20 14:50:18 +0700

    sorting with BubbleSort

---
+++ src/main/java/hiwijaya/algorithm/BubbleSort.java
@@ -0,0 +1,67 @@
+package hiwijaya.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * @author Happy Indra Wijaya
+ *
+ * BubbleSort
+ *
+ * - This algorithm works by repeatedly swapping the adjacent element if they are in wrong order.
+ * - Implemented "in-place", it means no need duplicated.
+ * - Complexity O(n2), it quite inefficient for sorting large data volumes.
+ * - The algorithm needs one more whole pass(iteration) without any swap to know it is sorted.
+ *
+ * Step:
+ * - go through until the last element and re-arrange its position with neighbor if they are in wrong order.
+ * - repeat until no more element to be swap.
+ *
+ * ref:
+ * - https://www.geeksforgeeks.org/bubble-sort/
+ * - https://howtodoinjava.com/algorithm/bubble-sort-java-example/
+ *
+ */
+public class BubbleSort {
+
+    private void sort(Integer[] arr){
+
+        int n = arr.length;
+
+        for(int i=0; i<n-1; i++){
+
+            boolean swapped = false;
+
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){      // asc/desc
+                    swap(arr, j, j+1);
+                    swapped = true;
+                }
+            }
+
+            if(!swapped){
+                break;
+            }
+
+        }
+
+    }
+
+    private void swap(Integer[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        BubbleSort app = new BubbleSort();
+
+        Integer[] input = new Integer[] {20, 10, 35, 2, 5, 35, 40, 60, 15, 30, 5, 10};
+
+        app.sort(input);
+
+        System.out.println(Arrays.toString(input));
+
+    }
+
+}
